09f8f32e1167ffd356dff5ed64477660ac6a0828
Temitope Abidoye <tiwajumai@gmail.com>
2023-03-16 12:46:02 -0700

    My first C programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Returns 0 Always
+ * */
+
+int main(void)
+{
+	printf("\"programming is like building a multilingual puzzle,");
+	return (0);
+}	
+
+
