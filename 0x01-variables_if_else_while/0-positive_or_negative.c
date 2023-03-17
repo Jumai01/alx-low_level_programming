eb3aeeef13b6c4d85dcd9126aa6dcae9910c3ba8
Temitope Abidoye <tiwajumai@gmail.com>
2023-03-17 13:57:34 -0700

    My first C variable

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry Level
+ * Description - Generates random n0s to check if it's-ve/+ve/0
+ *
+ * Return: Returns 0 Always
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else if (n < o)
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
