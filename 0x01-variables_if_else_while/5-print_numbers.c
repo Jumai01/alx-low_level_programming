39b130525726eb3b2bef66754379b83a6385b0d3
Temitope Abidoye <tiwajumai@gmail.com>
2023-03-17 18:21:49 -0700

    My first C variable

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry Level
+ * Description - Printsnumbers from o to  10
+ * Return: Return 0 Always
+ */
+
+int main(void)
+{
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+
+	{
+		printf("\n");
+	}
+	return (0);
+}
+
