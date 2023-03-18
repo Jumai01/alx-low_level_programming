79ee3f468e62131e9c18670d5608da79b58e3b60
Temitope Abidoye <tiwajumai@gmail.com>
2023-03-17 17:06:00 -0700

    My first C variable

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+/**
+ * main - Entry Level
+ * Description - Prints the alphabet in upper and lowwer case
+ * Return: Returns 0 Always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		putchar(i);
+	}
+
+	for (i = 'A' ; i <= 'Z' ; i++)
+	{
+		putchar(i);
+	}
+
+	{
+		putchar('\n');
+	}
+	return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
