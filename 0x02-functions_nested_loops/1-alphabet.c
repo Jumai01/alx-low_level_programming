1c0eb2a03a3c2992a55194ed7cf522f5a1734cda
Temitope Abidoye <tiwajumai@gmail.com>
2023-03-22 14:54:02 -0700

    My first C function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
  * main - Entry Level
- * Description - prints the alphabet, in lowercase, followed by a new line
- * Reurn: Returns 0 Always
+ * Description - functions that prints the aphabet in lowwer case
+ * Return: Returns 0 Always
 */
 void print_alphabet(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
-	
+	{
 		_putchar(c);
-	
-	
 		_putchar('\n');
+	}
 }
 
